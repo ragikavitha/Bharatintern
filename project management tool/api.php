ae48d177fdbfb5284589aeee70d0117a729b1507
ragikavitha <104778926+ragikavitha@users.noreply.github.com>
2023-10-05 07:55:28 +0530

    Update api.php

--- project management tool/api.php
+++ project management tool/api.php
@@ -1,1 +1,16 @@
+<?php
+header("Content-Type: application/json");
 
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $data = json_decode(file_get_contents("php://input"), true);
+    if (isset($data["projectName"])) {
+        $projectName = $data["projectName"];
+        // Save the project to a database or file (not implemented here)
+        echo json_encode(["success" => true]);
+    } else {
+        echo json_encode(["success" => false, "message" => "Project name not provided"]);
+    }
+} else {
+    echo json_encode(["success" => false, "message" => "Invalid request"]);
+}
+?>
