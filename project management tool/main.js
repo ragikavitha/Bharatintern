48dfa8664f859170c5bacf80b6056ecc21d55deb
ragikavitha <104778926+ragikavitha@users.noreply.github.com>
2023-10-05 07:59:12 +0530

    Update main.js

--- project management tool/main.js
+++ project management tool/main.js
@@ -1,1 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const projectForm = document.getElementById("create-project-form");
+    const projectNameInput = document.getElementById("project-name");
+    const projectList = document.getElementById("projects");
 
+    projectForm.addEventListener("submit", function (e) {
+        e.preventDefault();
+        const projectName = projectNameInput.value.trim();
+        if (projectName) {
+            createProject(projectName);
+            projectNameInput.value = "";
+        }
+    });
+
+    function createProject(projectName) {
+        const projectItem = document.createElement("li");
+        projectItem.textContent = projectName;
+        projectList.appendChild(projectItem);
+    }
+});
